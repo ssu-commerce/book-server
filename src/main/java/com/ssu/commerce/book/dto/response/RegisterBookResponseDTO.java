9abfbb43a02749e496c72027c959415df3cfdbe5
LeeSeongYeop <ys_lee1017@naver.com>
2022-06-18 17:50:51 +0900

    [feat] DTO 파일
    DTO 설계를 위한 파일

---
+++ src/main/java/com/ssu/commerce/book/dto/response/RegisterBookResponseDTO.java
@@ -0,0 +1,11 @@
+package com.ssu.commerce.book.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+public class RegisterBookResponseDTO {
+}
---
+++ src/main/java/com/ssu/commerce/book/dto/response/SearchBookResponseDTO.java
@@ -0,0 +1,10 @@
+package com.ssu.commerce.book.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+public class SearchBookResponseDTO {
+}

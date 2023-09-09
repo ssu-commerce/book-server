c2c4f9ca713e5a60283dbaa3b07b2256a7f742f0
JaeSung Choi <ky4171@naver.com>
2023-09-09 15:40:12 +0900

    Image Service 변경 내용

--- api/src/main/java/com/ssu/commerce/book/service/ImageService.java
+++ api/src/main/java/com/ssu/commerce/book/service/ImageService.java
@@ -5,12 +5,17 @@ import com.ssu.commerce.book.file.ImageStore;
 import com.ssu.commerce.book.model.Image;
 import com.ssu.commerce.book.persistence.ImageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.util.List;
 
 @Service
@@ -29,5 +34,23 @@ public class ImageService {
         imageRepository.saveAll(imageList);
 
         return imageList;
+    }
+
+    public String getFullPath(String fileName) {
+        return imageStore.getFullPath(fileName);
+    }
+
+
+    public Resource downloadImage(String imageId) throws IOException {
+        InputStream inputStream = getImageInputStream(imageId); // 이미지 파일의 InputStream 가져오기
+        return new InputStreamResource(inputStream);
+    }
+
+    public InputStream getImageInputStream(String imageId) throws IOException {
+        String imagePath = getFullPath(imageId);
+        Resource imageResource = new UrlResource("file:" + imagePath);
+
+        InputStream inputStream = imageResource.getInputStream();
+        return inputStream;
     }
 }

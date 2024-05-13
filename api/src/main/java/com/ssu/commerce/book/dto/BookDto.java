3075e1afc093f6a5bd056220a249b3e00a55c249
ccxz84 <ccxz84@naver.com>
2024-05-13 17:07:12 +0900

    [refactor] @EqualsAndHashCode 코드리뷰 반영

--- api/src/main/java/com/ssu/commerce/book/dto/BookDto.java
+++ api/src/main/java/com/ssu/commerce/book/dto/BookDto.java
@@ -2,6 +2,7 @@ package com.ssu.commerce.book.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 @Data
 @Builder
+@EqualsAndHashCode
 public class BookDto {
     private UUID id;
     private String title;
@@ -23,28 +25,4 @@ public class BookDto {
     private LocalDateTime publishDate;
     private String isbn;
     private UUID categoryId;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BookDto bookDto = (BookDto) o;
-        return Objects.equals(title, bookDto.title) &&
-                Objects.equals(content, bookDto.content) &&
-                Objects.equals(writer, bookDto.writer) &&
-                Objects.equals(price, bookDto.price) &&
-                Objects.equals(sharePrice, bookDto.sharePrice) &&
-                Objects.equals(comment, bookDto.comment) &&
-                Objects.equals(startBorrowDay, bookDto.startBorrowDay) &&
-                Objects.equals(endBorrowDay, bookDto.endBorrowDay) &&
-                Objects.equals(ownerId, bookDto.ownerId) &&
-                Objects.equals(publishDate, bookDto.publishDate) &&
-                Objects.equals(isbn, bookDto.isbn) &&
-                Objects.equals(categoryId, bookDto.categoryId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, content, writer, price, sharePrice, comment, startBorrowDay, endBorrowDay, ownerId, publishDate, isbn, categoryId);
-    }
 }

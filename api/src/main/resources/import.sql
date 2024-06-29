9cbf8ce0f763ecf0d213ea10e8df13d5b98b224e
JaeSung Choi <ky4171@naver.com>
2024-06-29 15:10:01 +0900

    #78 import.sql 수정

--- api/src/main/resources/import.sql
+++ api/src/main/resources/import.sql
@@ -1,1 +1,3 @@
 ALTER DATABASE test CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE test.category CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE test.book CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

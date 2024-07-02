ee1ffa82bf54d8328e21c2d4b9e0e3dec9c6525a
JaeSung Choi <ky4171@naver.com>
2024-07-02 20:49:37 +0900

    #78 테스트코드 불가 현상 수정

--- api/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-ALTER DATABASE test CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE test.category CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE test.book CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
